75f8c0e9e3958b731403ba64ff9ab9c105deef73
s-suresh <87108396+s-suresh@users.noreply.github.com>
2025-02-13 17:09:37 +0530

    Update untitled.py

--- untitled.py
+++ untitled.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import pickle
 import numpy as np
-import matplotlib.pyplot as plt  # Import Matplotlib  
+
 from PIL import Image  
 
 # Title of the app  
