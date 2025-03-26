bf0060b1c29a843ab549ed7e325a3fb4d1d72722
s-suresh <87108396+s-suresh@users.noreply.github.com>
2025-03-26 15:00:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from PIL import Image
 st.title("Body Measurements Input (Inches)")
 
 # Load the silhouette image
-image = Image.open("silhouette.png")  # Replace with the path to your silhouette image
+image = Image.open("pic02.png")  # Replace with the path to your silhouette image
 st.image(image, use_column_width=True)
 
 st.header("Enter Your Measurements (in inches)")
